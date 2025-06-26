8bd8a2c95ffc53c05fee676fabafb690b1a7e16e
amitrajankar99 <amit.rajankar99@gmail.com>
2025-06-26 00:05:34 -0700

    github secret actions added for connection strings

--- src/main/java/com/javacafe/ems/EmsBackendApplication.java
+++ src/main/java/com/javacafe/ems/EmsBackendApplication.java
@@ -3,11 +3,20 @@ package com.javacafe.ems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
 	}
+	
+	@PostConstruct
+	public void checkEnv() {
+	    String value = System.getenv("EVENTHUB_CONN_STRING");
+	    System.out.println("✅ ENV loaded: " + (value != null ? "Yes" : "No"));
+	}
+
 
 }
