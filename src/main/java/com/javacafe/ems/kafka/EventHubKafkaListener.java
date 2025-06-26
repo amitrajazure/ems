812c251c0448141e5b58f79b359d0b92fbcbfecb
amitrajankar99 <amit.rajankar99@gmail.com>
2025-06-25 21:46:49 -0700

    github secret actions added for connection strings

---
+++ src/main/java/com/javacafe/ems/kafka/EventHubKafkaListener.java
@@ -0,0 +1,17 @@
+package com.javacafe.ems.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventHubKafkaListener {
+	
+	EventHubKafkaListener() {
+		System.out.println("inside kafka constructor");
+	}
+	
+	@KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
+	public void consume(String message) {
+		System.out.println("Message consumed: " +message);
+	}
+}
